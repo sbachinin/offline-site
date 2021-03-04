074e1a2badb1ef9aee613d70aa62b3dc22386dbf
Sergey <sbachinin@yandex.ru>
2021-03-04 20:33:49 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -11,12 +11,9 @@ self.addEventListener('install', e => {
 
 self.addEventListener('fetch', event => {
     event.respondWith(
-        caches
-        .match(event.request)
-        .then(
-            function(response) {
+        caches.match(event.request)
+            .then(response => {
                 return response || fetch(event.request)
-            }
-        )
+            })
     )
 })
